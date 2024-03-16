41400bb4d634458c053b3e6e546a09c3df523b69
debug.icu <hajnxg@126.com>
2024-03-16 12:22:26 +0800

    feat: project create init

---
+++ src/main/java/icu/debug/ai/translate/AthenaTranscriptApplication.java
@@ -0,0 +1,13 @@
+package icu.debug.ai.translate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaTranscriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaTranscriptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/debug/ai/translate/AthenaTranscriptApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.debug.ai.translate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaTranscriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
